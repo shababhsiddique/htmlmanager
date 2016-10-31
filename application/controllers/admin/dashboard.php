04d5c0a75da23cf999dd76465f85ca7ba4354df3
shababhsiddique <shabab.h.siddique@gmail.com>
2016-10-31 20:17:42 +0600

    Project Started

---
+++ application/controllers/admin/dashboard.php
@@ -0,0 +1,61 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+
+
+/**
+ * Description of dashboard
+ *
+ * @author shabab
+ */
+class Dashboard extends CI_Controller {
+
+    /**
+     * Constructor
+     * @author Shabab Haider Siddique
+     */
+
+
+    public function __construct() {
+        parent::__construct();
+
+        // Check whether admin is logged in
+        $admin_logged_in = $this->session->userdata('admin_logged_in');
+        if (!isset($admin_logged_in) || $admin_logged_in == false) {
+            redirect('admin/login');
+        } 
+
+      
+    }
+
+
+    public function index(){
+        echo ":hi";
+    }
+    /**
+     * Log out and Remove Session values
+     * @author Shabab Haider Siddique
+     * @param --- none
+     * @return --- none
+     * date --- 10/11/2012 (mm/dd/yyyy  )
+     */
+    public function logout() {
+
+        $this->session->sess_destroy();
+        $this->session->unset_userdata('admin_user_name');
+        $this->session->unset_userdata('admin_logged_in');
+        $this->session->unset_userdata('admin_name');
+        $this->session->unset_userdata('admin_privilage_level');
+        $this->session->unset_userdata('admin_user_id');
+
+        $sessionData = setmessage("Logged Out", "from admin ", "info");
+        $this->session->set_userdata($sessionData);
+
+//        session_destroy();
+
+        redirect('admin', 'refresh');
+    }
+
+}
